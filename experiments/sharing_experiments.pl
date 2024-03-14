d1610afa4368818c4d1bd69d279f32b804f9e18b
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-03-14 15:37:10 +0100

    Add experiment of Example 7.13 of "Optimality in Dependent Analysis of Sharing"

--- experiments/sharing_experiments.pl
+++ experiments/sharing_experiments.pl
@@ -9,7 +9,7 @@
 % We use t(U) instead of U to bypass share optimization when goal and
 % head are variants.
 example1(t(U), V, W) :-
-    % [U, V, W] is not possible here
+    % [U, V, W] is not possible here (need -fmgu_sh_optimize=on)
     nothing.
 
 :- entry example2(X, Y, Z): mshare([[X, Y],[X, Z]]).
@@ -17,7 +17,7 @@ example1(t(U), V, W) :-
 % Example 6.3 "Optimality in Dependent Analysis of Sharing"
 
 example2(t(U, V), H, K) :-
-    % [H, K, U] is not possible here
+    % [H, K, U] is not possible here (need -fmgu_sh_optimize=on)
     nothing.
 
 % Example 6.19 "Optimality in Dependent Analysis of Sharing"
@@ -27,5 +27,22 @@ example2(t(U, V), H, K) :-
 example3(f(U, H), f(U, K), S, T) :-
     % [S, T, H] is not possible here
     nothing.
+
+:- entry example4(X, Y, Z) : mshare([[X], [Y], [Z]]).
+
+% Example 7.13 "Optimality in Dependent Analysis of Sharing"
+
+example4(X, Y, Z) :-
+    example4bis(t(X), t(Y), t(Z))
+    % [X, Y, Z] is not possible here (need -fmgu_sh_optimize=on)
+    .
+
+% We refine the result of the sharing analysis to overcome the low precision
+% of forward unification in share.
+
+:- trust pred example4bis(X, Y, Z): mshare([[X], [Y], [Z]]) => mshare([[X, Y], [Y, Z]]).
+
+example4bis(X, Y, Z) :-
+    Y = f(X, Z).
 
 nothing.

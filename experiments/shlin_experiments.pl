056ab5fe47a46544c5041f475f44e556c2416b99
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-03-17 21:55:50 +0100

    Add experiment showing advantage of removing independence check.

--- experiments/shlin_experiments.pl
+++ experiments/shlin_experiments.pl
@@ -11,11 +11,20 @@ example1 :-
 
 p(t(U,U)).
 
-:- entry example2(X): (linear([X])).
+:- entry example2(X): linear([X]).
 
 example2(X) :-
     % here X should not BE linear but shfrlin does not infer this property if
     % we do not add the assertion var(X).
+    nothing.
+
+:- entry example3(X, X1, X2, Y, Y1, Y2, Z):
+    (mshare([[X,X1], [X, X2], [X, Y, Z], [Y, Y1], [Y, Y2]]),
+     linear([X, X1, X2, Y, Y1, Y2, Z])).
+
+example3(X, X1, X2, Y, Y1, Y2, Z) :-
+    X = Y,
+    % [X,X1,X2,Y,Y2] is not possible ma proving it requires option mgu_shlin_noindcheck=on
     nothing.
 
 nothing.

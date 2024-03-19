83293dbff1fc7fde033c2487785c056226512b1e
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-03-19 23:01:11 +0100

    Add new flag controlling the use of the optimal match.

--- experiments/analysis.sh
+++ experiments/analysis.sh
@@ -3,9 +3,10 @@
 FILE=shlin_experiments.pl
 OPTIONS="-ftypes=none -fcollapse_ai_vers=off -fpp_info=on"
 
-ciaopp  -o as_shlin_opt.pl -A $FILE -fmodes=as_shlin $OPTIONS -fmgu_shlin_optimize=optimal
-ciaopp  -o as_shlin_noindcheck.pl -A $FILE -fmodes=as_shlin $OPTIONS -fmgu_shlin_optimize=noindcheck
-ciaopp  -o as_shlin_noopt.pl -A $FILE -fmodes=as_shlin $OPTIONS -fmgu_shlin_optimize=off
+ciaopp  -o as_shlin_optopt.pl -A $FILE -fmodes=as_shlin $OPTIONS -fmgu_shlin_optimize=optimal -fmatch_shlin_optimize=optimal
+ciaopp  -o as_shlin_opt.pl -A $FILE -fmodes=as_shlin $OPTIONS -fmgu_shlin_optimize=optimal -fmatch_shlin_optimize=off
+ciaopp  -o as_shlin_noindcheck.pl -A $FILE -fmodes=as_shlin $OPTIONS -fmgu_shlin_optimize=noindcheck -fmatch_shlin_optimize=optimal
+ciaopp  -o as_shlin_noopt.pl -A $FILE -fmodes=as_shlin $OPTIONS -fmgu_shlin_optimize=off -fmatch_shlin_optimize=optimal
 ciaopp  -o as_sharing_opt.pl -A $FILE -fmodes=as_sharing $OPTIONS -fmgu_sh_optimize=on
 ciaopp -o as_sharing_noopt.pl -A $FILE -fmodes=as_sharing $OPTIONS -fmgu_sh_optimize=off
 ciaopp -o share.pl -A $FILE -fmodes=share_amgu $OPTIONS

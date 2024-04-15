32f523fc7998ca935b232f5c5d7eb706252b846c
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-04-15 20:15:15 +0200

    Fix typos in sharing_experiments.

--- experiments/sharing_experiments.pl
+++ experiments/sharing_experiments.pl
@@ -2,7 +2,7 @@
 
 :- set_prolog_flag(single_var_warnings, off).
 
-% Example 6.2 "Optimality in Dependent Analysis of Sharing"
+% Example 6.2 "Optimality in Goal Dependent Analysis of Sharing"
 
 :- entry example1(X, Y, Z): mshare([[X, Y],[Y, Z]]).
 
@@ -14,13 +14,13 @@ example1(t(U), V, W) :-
 
 :- entry example2(X, Y, Z): mshare([[X, Y],[X, Z]]).
 
-% Example 6.3 "Optimality in Dependent Analysis of Sharing"
+% Example 6.3 "Optimality in Goal Dependent Analysis of Sharing"
 
 example2(t(U, V), H, K) :-
     % [H, K, U] is not possible here (need -fmgu_sh_optimize=on)
     nothing.
 
-% Example 6.19 "Optimality in Dependent Analysis of Sharing"
+% Example 6.19 "Optimality in Goal Dependent Analysis of Sharing"
 
 :- entry example3(X, Y, Z, W): mshare([[X, W], [X, Z], [Y, W], [Y, Z]]).
 
@@ -30,18 +30,18 @@ example3(f(U, H), f(U, K), S, T) :-
 
 :- entry example4(X, Y, Z) : mshare([[X], [Y], [Z]]).
 
-% Example 7.13 "Optimality in Dependent Analysis of Sharing"
+% Example 7.3 "Optimality in Goal Dependent Analysis of Sharing"
 
 example4(X, Y, Z) :-
     example4bis(t(X), t(Y), t(Z)).
-    % [X, Y, Z] is not possible here (need -fmgu_sh_optimize=on)
+    % [X, Y, Z] is not possible here (need -fmgu_sh_optimize=on and -fas_use_match=on)
 
 % We refine the result of the sharing analysis to overcome the low precision
 % of forward unification in share.
 
-:- trust pred example4bis(X, Y, Z): mshare([[X], [Y], [Z]]) => mshare([[X, Y], [Y, Z]]).
+:- trust pred example4bis(U, V, W): mshare([[U], [V], [W]]) => mshare([[U, V], [V, W]]).
 
-example4bis(X, Y, Z) :-
-    Y = f(X, Z).
+example4bis(U, V, W) :-
+    V = f(U, W).
 
 nothing.

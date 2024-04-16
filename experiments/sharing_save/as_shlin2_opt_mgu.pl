98a892e78d156bf35894fd06dc6d0c410b391cd9
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-04-16 16:53:02 +0200

    Add benchmark result for ShLin2 and sharing experiment.

---
+++ experiments/sharing_save/as_shlin2_opt_mgu.pl
@@ -0,0 +1,118 @@
+:- module(_2,[],[assertions,nativeprops]).
+
+:- set_prolog_flag(single_var_warnings,off).
+
+:- entry example1(X,Y,Z)
+   : mshare([X,Y,Z],[[X,Y],[Y,Z]]).
+
+:- true pred example1(X,Y,Z)
+   : ( mshare([[X,Y],[Y,Z]]),
+       shlin2([([X,Y],[]),([Y,Z],[])]) )
+   => ( mshare([[X,Y],[Y,Z]]),
+        shlin2([([X,Y],[]),([Y,Z],[])]) ).
+
+example1(t(U),V,W) :-
+    true((
+        mshare([[V,W],[V,U]]),
+        shlin2([([V,W],[]),([V,U],[])])
+    )),
+    nothing,
+    true((
+        mshare([[V,W],[V,U]]),
+        shlin2([([V,W],[]),([V,U],[])])
+    )).
+
+:- entry example2(X,Y,Z)
+   : mshare([X,Y,Z],[[X,Y],[X,Z]]).
+
+:- true pred example2(X,Y,Z)
+   : ( mshare([[X,Y],[X,Z]]),
+       shlin2([([X,Y],[]),([X,Z],[])]) )
+   => ( mshare([[X,Y],[X,Z]]),
+        shlin2([([X,Y],[]),([X,Z],[])]) ).
+
+example2(t(U,V),H,K) :-
+    true((
+        mshare([[H,U],[H,U,V],[H,V],[K,U],[K,U,V],[K,V]]),
+        shlin2([([H,U],[]),([H,U,V],[]),([H,V],[]),([K,U],[]),([K,U,V],[]),([K,V],[])])
+    )),
+    nothing,
+    true((
+        mshare([[H,U],[H,U,V],[H,V],[K,U],[K,U,V],[K,V]]),
+        shlin2([([H,U],[]),([H,U,V],[]),([H,V],[]),([K,U],[]),([K,U,V],[]),([K,V],[])])
+    )).
+
+:- entry example3(X,Y,Z,W)
+   : mshare([X,Y,Z,W],[[X,W],[X,Z],[Y,W],[Y,Z]]).
+
+:- true pred example3(X,Y,Z,W)
+   : ( mshare([[X,Z],[X,W],[Y,Z],[Y,W]]),
+       shlin2([([X,Z],[]),([X,W],[]),([Y,Z],[]),([Y,W],[])]) )
+   => ( mshare([[X,Y,Z],[X,Y,Z,W],[X,Y,W],[X,Z],[X,W],[Y,Z],[Y,W]]),
+        shlin2([([X,Y,Z],[]),([X,Y,Z,W],[]),([X,Y,W],[]),([X,Z],[]),([X,W],[]),([Y,Z],[]),([Y,W],[])]) ).
+
+example3(f(U,H),f(U,K),S,T) :-
+    true((
+        mshare([[S,T,U],[S,T,U,H],[S,T,U,H,K],[S,T,U,K],[S,U],[S,U,H],[S,U,H,K],[S,U,K],[S,H],[S,K],[T,U],[T,U,H],[T,U,H,K],[T,U,K],[T,H],[T,K]]),
+        shlin2([([S,T,U],[]),([S,T,U,H],[]),([S,T,U,H,K],[]),([S,T,U,K],[]),([S,U],[]),([S,U,H],[]),([S,U,H,K],[]),([S,U,K],[]),([S,H],[]),([S,K],[]),([T,U],[]),([T,U,H],[]),([T,U,H,K],[]),([T,U,K],[]),([T,H],[]),([T,K],[])])
+    )),
+    nothing,
+    true((
+        mshare([[S,T,U],[S,T,U,H],[S,T,U,H,K],[S,T,U,K],[S,U],[S,U,H],[S,U,H,K],[S,U,K],[S,H],[S,K],[T,U],[T,U,H],[T,U,H,K],[T,U,K],[T,H],[T,K]]),
+        shlin2([([S,T,U],[]),([S,T,U,H],[]),([S,T,U,H,K],[]),([S,T,U,K],[]),([S,U],[]),([S,U,H],[]),([S,U,H,K],[]),([S,U,K],[]),([S,H],[]),([S,K],[]),([T,U],[]),([T,U,H],[]),([T,U,H,K],[]),([T,U,K],[]),([T,H],[]),([T,K],[])])
+    )).
+
+:- entry example4(X,Y,Z)
+   : mshare([X,Y,Z],[[X],[Y],[Z]]).
+
+:- true pred example4(X,Y,Z)
+   : ( mshare([[X],[Y],[Z]]),
+       shlin2([([X],[]),([Y],[]),([Z],[])]) )
+   => ( mshare([[X,Y],[X,Y,Z],[Y,Z]]),
+        shlin2([([X,Y],[]),([X,Y,Z],[]),([Y,Z],[])]) ).
+
+example4(X,Y,Z) :-
+    true((
+        mshare([[X],[Y],[Z]]),
+        shlin2([([X],[]),([Y],[]),([Z],[])])
+    )),
+    example4bis(t(X),t(Y),t(Z)),
+    true((
+        mshare([[X,Y],[X,Y,Z],[Y,Z]]),
+        shlin2([([X,Y],[]),([X,Y,Z],[]),([Y,Z],[])])
+    )).
+
+%% %% :- trust pred example4bis(U,V,W)
+%% %%    : mshare([U,V,W],[[U],[V],[W]])
+%% %%    => mshare([U,V,W],[[U,V],[V,W]]).
+
+:- check calls example4bis(U,V,W)
+   : mshare([U,V,W],[[U],[V],[W]]).
+
+:- trust success example4bis(U,V,W)
+   : mshare([U,V,W],[[U],[V],[W]])
+   => mshare([U,V,W],[[U,V],[V,W]]).
+
+:- true pred example4bis(U,V,W)
+   : ( (U=t(_A)), (V=t(_B)), (W=t(_C)),
+       mshare([[_A],[_B],[_C]]),
+       shlin2([([_A],[]),([_B],[]),([_C],[])]) )
+   => ( mshare([[_A,_B],[_B,_C]]),
+        shlin2([([_A,_B],[]),([_B,_C],[])]) ).
+
+example4bis(U,V,W) :-
+    true((
+        mshare([[U],[V],[W]]),
+        shlin2([([U],[]),([V],[]),([W],[])])
+    )),
+    V=f(U,W),
+    true((
+        mshare([[U,V],[U,V,W],[V,W]]),
+        shlin2([([U,V],[]),([U,V,W],[]),([V,W],[])])
+    )).
+
+:- true pred nothing.
+
+nothing.
+
+

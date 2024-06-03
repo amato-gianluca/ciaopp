d2446c706702e89ab9d2a14b3b9b026a44d238e2
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-06-03 15:54:43 +0200

    Make time report script generate boxplots.

--- benchmarks/report_time.py
+++ benchmarks/report_time.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
@@ -8,23 +10,41 @@ import re
 import csv
 
 def show_graph(df, domains):
-    fig, ax = plt.subplots(layout='constrained')
+    _, ax = plt.subplots(layout='constrained')
     width = 0.8/len(domains)
     x = np.arange(len(df['program']))
     for i, domain in enumerate(domains):
-        ax.bar(x + i*width, df[domain], width=width,  label = domain)
+        ax.bar(x + i*width, df[domain], width=width, label = domain)
     ax.set_title('Analysis execution time')
+    ax.set_xlabel(domains)
     ax.set_ylabel('Time (ms)')
     ax.legend(loc='upper left', ncols=3)
     ax.set_ylim(0, 250)
+
+def show_hist(df, domains):
+    _, axs = plt.subplots(ncols=len(domains))
+    bins=10
+    for i, dom in enumerate(domains):
+        logbins = np.logspace(np.log10(np.min(df[dom])),np.log10(np.max(df[dom])),bins+1)
+        axs[i].hist(df[dom], bins=logbins)
+        axs[i].set_xscale('log')
+        axs[i].set_title(dom)
+
+def show_boxplot(df, domains, outliers):
+    fig, ax = plt.subplots()
+    ax.boxplot(df[domains], showfliers=outliers, labels=domains)
+    ax.set_title('Analysis execution time')
+    ax.set_ylabel('Time (ms)')
+    return fig
 
 PROGRAM_STRING = 'Analyzing with aim: '
 TIME_STRING = '{analyzed by plai in '
 
 parser = argparse.ArgumentParser(description="Process log files for benchmakrs and report about execution time")
-parser.add_argument('-d', '--dir', help='directory of benchmark results')
-parser.add_argument('-t', '--table', action='store_true', help='generate table with all results')
-parser.add_argument('-g', '--graph', action='store_true', help='generate graph')
+parser.add_argument('-d', '--dir', help='directory where benchmark results are stored')
+parser.add_argument('-t', '--table', action='store_true', help='generate table with all results on stdout')
+parser.add_argument('-b', '--boxplot', action='store_true', help='generate boxplots')
+parser.add_argument('-o', '--output', action='store', default='', help='base filename for PDF files')
 args = parser.parse_args()
 
 table = []
@@ -58,8 +78,21 @@ if args.table:
             line.append(table_row.get(domain, ''))
         csvwriter.writerow(line)
 
-if args.graph:
-    show_graph(df, ['share', 'as_sharing_noopt_mgu', 'as_sharing_opt'])
-    show_graph(df, ['shfrlin', 'as_shlin_noopt_mgu', 'as_shlin_opt_opt'])
-    show_graph(df, ['as_shlin2_noopt_mgu', 'as_shlin2_opt'])
-    plt.show()
+if args.boxplot:
+    df_selection = df.dropna()
+    #df_selection= df[df.apply(lambda row: all(float(column) <= 100 for column in row[1:]), axis=1)]
+    fig1 = show_boxplot(df_selection, ['share', 'as_sharing_noopt_mgu', 'as_sharing_noopt', 'as_sharing_opt'], False)
+    fig2 = show_boxplot(df_selection, ['shfrlin', 'as_shlin_noopt_mgu', 'as_shlin_noopt', 'as_shlin_opt_opt'], False)
+    fig3 = show_boxplot(df_selection, ['as_shlin2_noopt_mgu', 'as_shlin2_noopt', 'as_shlin2_opt'], False)
+    if args.output == '':
+        plt.show()
+    else:
+        filename = args.output
+        fig1.savefig(filename + '1.pdf')
+        fig2.savefig(filename + '2.pdf')
+        fig3.savefig(filename + '3.pdf')
+
+#show_graph(df, ['share', 'as_sharing_noopt_mgu', 'as_sharing_opt'])
+#show_graph(df, ['shfrlin', 'as_shlin_noopt_mgu', 'as_shlin_opt_opt'])
+#show_graph(df, ['as_shlin2_noopt_mgu', 'as_shlin2_opt'])
+#show_hist(df, ['share', 'as_sharing_noopt_mgu', 'as_sharing_noopt', 'as_sharing_opt'])

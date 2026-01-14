3d8b51ec47aa1d6208d88b9837739dca3170700b
Gianluca Amato <gianluca.amato.74@gmail.com>
2026-01-14 10:42:14 +0100

    Comment profiler directives.

--- domains/as_sharing.pl
+++ domains/as_sharing.pl
@@ -1,7 +1,7 @@
 :- module(as_sharing, [], [assertions, basicmodes, nativeprops, indexer]).
 
-:- use_package(profilercc).
-:- cost_center mgu/4.
+%:- use_package(profilercc).
+%:- cost_center mgu/4.
 
 % :- use_package(debug).
 % :- use_package(rtchecks).

542a7a4d91ab692c204d15834c8e57dcd1629337
Gianluca Amato <gianluca.amato.74@gmail.com>
2026-01-30 13:14:00 +0100

    Reformat test annotations.

--- domains/as_aux.pl
+++ domains/as_aux.pl
@@ -25,13 +25,17 @@ This module is in common among all domains in the as_* collection.
 % ASSERTIONS
 %-------------------------------------------------------------------------
 
-:- prop memberof(+L, +T) # "@var{T} is a member of list @var{L}".
+:- prop memberof(+L, +T): list(L) + is_det
+   # "@var{T} is a member of list @var{L}".
+
 :- export(memberof/2).
 
 memberof(L, T)
    :- member(T, L).
 
-:- prop list_nonempty(+T,+L) # "@var{L} is a non-empty list of elements of type T".
+:- prop list_nonempty(+T,+L) + is_det
+   # "@var{L} is a non-empty list of elements of type T".
+
 :- export(list_nonempty/2).
 :- test list_nonempty(int, [1, 2, 3]) + (not_fails, is_det).
 :- test list_nonempty(int, [3, 2, 1]) + (not_fails, is_det).
@@ -44,7 +48,9 @@ list_nonempty(T, L) :-
 
 :- push_prolog_flag(read_hiord, on).
 
-:- prop ordlist_nonempty(+T,+L) # "@var{L} is a non-empty ordered list of elements of type T".
+:- prop ordlist_nonempty(+T,+L) + is_det
+   # "@var{L} is a non-empty ordered list of elements of type T".
+
 :- meta_predicate ordlist_nonempty(pred(1),+).
 :- export(ordlist_nonempty/2).
 :- test ordlist_nonempty(int, [1, 2, 3]) + (not_fails, is_det).
@@ -61,8 +67,9 @@ ordlist_nonempty(T, [X1,X2|Xs]) :-
 
 :- pop_prolog_flag(read_hiord).
 
-:- prop ordlist(+T, +S)
+:- prop ordlist(+T, +S) + is_det
    # "@var{S} is an ordered list of elements of type T".
+
 :- meta_predicate ordlist(pred(1),+).
 :- export(ordlist/2).
 :- test ordlist(int, [1, 2, 3]) + (not_fails, is_det).
@@ -74,8 +81,9 @@ ordlist(_T, []).
 ordlist(T, S) :-
    ordlist_nonempty(T, S).
 
-:- prop ordlist(+S)
+:- prop ordlist(+S) + is_det
    # "@var{S} is an ordered list".
+
 :- meta_predicate ordlist(pred(1),?).
 :- export(ordlist/1).
 :- test ordlist([1, 2, 3]) + (not_fails, is_det).
@@ -86,8 +94,9 @@ ordlist(T, S) :-
 ordlist(S) :-
    ordlist(term, S).
 
-:- prop independent_from(?Term1, ?Term2)
+:- prop independent_from(?Term1, ?Term2) + is_det
    # "@var{Term1} and @var{Term2} do not share variables".
+
 :- export(independent_from/2).
 :- test independent_from([X, Z], [U]) + (not_fails, is_det).
 :- test independent_from([X, Z], [X]) + (fails, is_det).
@@ -97,8 +106,9 @@ independent_from(Term1, Term2) :-
    varset(Term2, Vars2),
    ord_disjoint(Vars1, Vars2).
 
-:- prop superset_vars_of(?Term1, ?Term2)
+:- prop superset_vars_of(?Term1, ?Term2) + is_det
    # "@var{Term2} has a superset of the variables of @var{Term1}".
+
 :- export(superset_vars_of/2).
 :- test superset_vars_of([X, Z], [X, Y, Z]) + (not_fails, is_det).
 :- test superset_vars_of([X, Z], [X, Y]) + (fails, is_det).
@@ -109,8 +119,9 @@ superset_vars_of(Term1, Term2) :-
    varset(Term2, Vars2),
    ord_subset(Vars1, Vars2).
 
-:- prop same_vars_of(?Term1, ?Term2)
+:- prop same_vars_of(?Term1, ?Term2) + is_det
    # "@var{Term1} and @var{Term2} have the same variables".
+
 :- export(same_vars_of/2).
 :- test same_vars_of([X, Z], [X, Y, Z]) + (fails, is_det).
 :- test same_vars_of([X, Z], [X, Y]) + (fails, is_det).
@@ -121,12 +132,13 @@ same_vars_of(Term1, Term2) :-
    varset(Term2, Vars2),
    Vars1 == Vars2.
 
-:- prop predicate_of(+Goal,+Pred)
-   : cgoal * ivar => atm(Pred)
+:- prop predicate_of(+Goal,?Pred): cgoal * term => atm(Pred)
    # "@var{Pred} is the predicate of @var{Goal}".
+
 :- export(predicate_of/2).
 :- test predicate_of('m:example'(a, X), 'm:example/2') + (not_fails, is_det).
 :- test predicate_of(example(a, X), 'example/2') + (not_fails, is_det).
+:- test predicate_of(example(a, X), Y) => (Y='example/2') + (not_fails, is_det).
 
 predicate_of(Goal, Pred) :-
    functor(Goal, Name, Arity),
@@ -134,8 +146,9 @@ predicate_of(Goal, Pred) :-
    atom_number(N, Arity),
    atom_concat(Pred1, N, Pred).
 
-:- prop multiplicity(?X)
+:- prop multiplicity(?X) + is_det
    # "@var{X} is a non negative integer or the atom 'inf'".
+
 :- export(multiplicity/1).
 :- test multiplicity(3) + (not_fails, is_det).
 :- test multiplicity(inf) + (not_fails, is_det).
@@ -155,6 +168,7 @@ multiplicity(X) :- nnegint(X).
    : ordlist * ordlist * term => memberof([yes,no], Result)
    + (not_fails, is_det)
    # "If Set1 and Set2 have at least an element in common, then Result=yes. Otherwise Result=no.".
+
 :- export(ord_test_intersect/3).
 :- test ord_test_intersect([X, hello, world], [X], Result) => (Result == yes) + (not_fails, is_det).
 :- test ord_test_intersect([X, hello, world], [Y], Result) => (Result == no) + (not_fails, is_det).
@@ -162,19 +176,18 @@ multiplicity(X) :- nnegint(X).
 ord_test_intersect(Set1, Set2, Result) :-
    ord_intersect(Set1, Set2) -> Result = yes ; Result = no.
 
-:- pred if_not_nil(+List, +Token, -List1, ?List2)
-   :: list(List2) : list * term * ivar * term  => (term(List1), term(List2))
-   + (not_fails, is_det)
-   # "If @var{List} is empty, then @var{List1} is equal to @var{List2}, otherwise
-   @var(List1} is obtained by prepending @var{Token} to @var{List2}.".
+:- pred if_not_nil(+List, +Token, -List1, ?List2): list * term * ivar * term + (not_fails, is_det)
+   # "If @var{List} is empty, then @var{List1} is equal to @var{List2}, otherwise @var(List1} is obtained by prepending
+   @var{Token} to @var{List2}.".
+
 :- export(if_not_nil/4).
 
 if_not_nil([], _, Xs, Xs) :- !.
 if_not_nil(_, X, [X|Xs], Xs).
 
-:- prop unifier_no_cyclic(+Unifier)
-   : unifier
+:- prop unifier_no_cyclic(+Unifier): unifier + is_det
    # "@var{Unifier} is a unifier without cyclic bindings".
+
 :- export(unifier_no_cyclic/1).
 :- test unifier_no_cyclic(U): (U = [X=f(Y)]) + (not_fails, is_det).
 :- test unifier_no_cyclic(U): (U = [X=f(X)]) + (fails, is_det).
@@ -185,12 +198,11 @@ unifier_no_cyclic([X = T|Rest]) :-
    ord_test_member(Vt, X, no),
    unifier_no_cyclic(Rest).
 
-:- pred unifiable_with_occurs_check(?T1, ?T2, -Unifier)
-   : term * term * ivar => unifier(Unifier)
-   + is_det
+:- pred unifiable_with_occurs_check(?T1, ?T2, -Unifier): term * term * ivar => unifier(Unifier) + is_det
    # "@var{Unifier} is the unifier of @var{T1} and @var{T2} with occurs check".
+
 :- export(unifiable_with_occurs_check/3).
-:- test unifiable_with_occurs_check(T1, T2, U): (T1 = f(X), T2 = f(Y)) + (not_fails, is_det).
+:- test unifiable_with_occurs_check(T1, T2, U): (T1 = f(X), T2 = f(Y)) => (U=[Y=X]) + (not_fails, is_det).
 :- test unifiable_with_occurs_check(T1, T2, U): (T1 = f(X), T2 = X) + (fails, is_det).
 
 unifiable_with_occurs_check(T1, T2, Unifier) :-
@@ -204,13 +216,15 @@ unifiable_with_occurs_check(T1, T2, Unifier) :-
 :- pred chiMax(+Sh, +Lin, +Bt, -Mul)
    : ordlist(var) * ordlist(var) * isbag(var) * ivar => multiplicity(Mul)
    + (not_fails, is_det)
-   # "@var{Mul} is the multiplicity of the term represented by the bag of variables @var{Bt}
-   w.r.t. the sharing group @var{Sh} and linear variables in @var{Lin}.".
+   # "@var{Mul} is the multiplicity of the term represented by the bag of variables @var{Bt}  w.r.t. the sharing group
+   @var{Sh} and linear variables in @var{Lin}.".
+
 :- pred chiMax(+Sh, +Lin, +Bt, +Mul)
    : ordlist(var) * ordlist(var) * isbag(var) * multiplicity => multiplicity(Mul)
-   + (is_det)
-   # "Determines whether @var{Mul} is the multiplicity of the term represented by the bag of
-   variables @var{Bt} w.r.t. the sharing group @var{Sh} and linear variables in @var{Lin}.".
+   + is_det
+   # "Determines whether @var{Mul} is the multiplicity of the term represented by the bag of variables @var{Bt} w.r.t.
+   the sharing group @var{Sh} and linear variables in @var{Lin}.".
+
 :- export(chiMax/4).
 :- test chiMax(Sh, Lin, Bt, Mul): (Sh = [X], Lin=[X,Y], Bt = [X-1,Y-2,Z-3]) => (Mul = 1)+ (not_fails, is_det).
 :- test chiMax(Sh, Lin, Bt, Mul): (Sh = [X,Y,Z], Lin=[X,Y], Bt = [X-1,Y-2,Z-1]) => (Mul = inf) + (not_fails, is_det).
@@ -241,16 +255,13 @@ chiMax0([X|RestO], Lin, [Y-N|RestBt], Mul0, Mul) :-
          chiMax0([X|RestO], Lin, RestBt, Mul0, Mul)
    ).
 
-:- pred chiMin(+Sh, +Bt, -Mul)
-   : ordlist(var) * isbag(var) * ivar => multiplicity(Mul)
-   + (not_fails, is_det)
-   # "@var{Mul} is the multiplicity of the term represented by the bag of variables @var{Bt}
-   w.r.t. the sharing group @var{Sh}, when all variables are assumed to be linear".
-:- pred chiMin(+Sh, +Bt, +Mul)
-   : ordlist(var) * isbag(var) * multiplicity
-   + (is_det)
-   # "Determines whether @var{Mul} is the multiplicity of the term represented by the bag
-   of variables @var{Bt} w.r.t. the sharing group @var{Sh}, when all variables are assumed to be linear".
+:- pred chiMin(+Sh, +Bt, -Mul): ordlist(var) * isbag(var) * ivar => multiplicity(Mul) + (not_fails, is_det)
+   # "@var{Mul} is the multiplicity of the term represented by the bag of variables @var{Bt} w.r.t. the sharing group
+   @var{Sh}, when all variables are assumed to be linear".
+
+:- pred chiMin(+Sh, +Bt, +Mul): ordlist(var) * isbag(var) * multiplicity + (is_det)
+   # "Determines whether @var{Mul} is the multiplicity of the term represented by the bag of variables @var{Bt} w.r.t.
+   the sharing group @var{Sh}, when all variables are assumed to be linear".
 
 :- export(chiMin/3).
 :- test chiMin(Sh, Bt, Mul): (Sh = [], Bt = [X-1,Y-2,Z-3]) => (Mul = 0) + (not_fails, is_det).
@@ -281,8 +292,9 @@ chiMin0([X|RestSh], [Y-N|RestBt], Mul0, Mul) :-
 :- pred linearizable(+Sh, +Bt)
    : ordlist(var) * isbag(var)
    + is_det
-   # "Determines if the concretization of the term represented by the bag of variables
-   @var{Bt} is linear w.r.t. the sharing group @var{Sh}, assuming all variables are linear.".
+   # "Determines if the concretization of the term represented by the bag of variables @var{Bt} is linear w.r.t. the
+   sharing group @var{Sh}, assuming all variables are linear.".
+
 :- export(linearizable/2).
 :- test linearizable(Sh, Bt): (Sh = [X], Bt = [X-1,Y-2,Z-3]) + (not_fails, is_det).
 :- test linearizable(Sh, Bt): (Sh = [X,Y,Z], Bt = [X-1,Y-1,Z-1]) + (fails, is_det).
@@ -301,11 +313,10 @@ linearizable([X|RestSh], [Y-N|RestBag]) :-
          linearizable([X|RestSh], RestBag)
    ).
 
-:- pred grounding(+Sh, +Bt)
-   : ordlist(var) * isbag(var)
-   + is_det
-   # "Determines if the concretization of the term represented by the bag of variables @var{Bt} is ground
-   w.r.t. the sharing group @var{Sh}.".
+:- pred grounding(+Sh, +Bt): ordlist(var) * isbag(var) + is_det
+   # "Determines if the concretization of the term represented by the bag of variables @var{Bt} is ground w.r.t. the
+   sharing group @var{Sh}.".
+
 :- export(grounding/2).
 :- test grounding(Sh, Bt): (Sh = [X, Y], Bt = [X-1,Y-2,Z-3]) + (fails, is_det).
 :- test grounding(Sh, Bt): (Sh = [X], Bt = [Y-1,Z-1]) + (not_fails, is_det).
